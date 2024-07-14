fb80d9d5bea72fff80d9a7c71d92d29cec68b526
Htun Htun Htet <ryanhhh91@gmail.com>
2024-07-13 18:54:19 -0600

    introduce header with navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header.jsx';
+import Header from './componenets/Header.jsx';
 import {createRoot} from 'react-dom/client';
 
 const App = () => {
